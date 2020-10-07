a7d9036747b4edd210669f4c55d08c9e89b8790e
Nhat Nguyen <leonadvice@Nhats-MacBook-Pro.local>
2020-10-07 10:52:09 +0700

    add refersh access to res

--- passport-init.js
+++ passport-init.js
@@ -1,10 +1,10 @@
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
 
 passport.serializeUser(function (user, done) {
-  console.log('THIS RUN IN SER');
+  console.log("THIS RUN IN SER");
   done(null, user);
-  console.log('----------------------- END OF SER');
+  console.log("----------------------- END OF SER");
 });
 
 passport.deserializeUser(function (user, done) {
@@ -15,17 +15,21 @@ passport.use(
   new GoogleStrategy(
     {
       clientID:
-        '1064798903066-89jln5roavgbleqlbt9ahl0ebpo9ae3u.apps.googleusercontent.com',
-      clientSecret: 'RT2-EteVF6OsM2cFtlBuN5xB',
-      callbackURL: 'http://localhost:4000/google/callback'
+        "1064798903066-89jln5roavgbleqlbt9ahl0ebpo9ae3u.apps.googleusercontent.com",
+      clientSecret: "RT2-EteVF6OsM2cFtlBuN5xB",
+      callbackURL: "http://localhost:4000/google/callback",
     },
     (accessToken, refreshToken, profile, cb) => {
       //Check if email exist
       //if not create user with email and name, then must ask for name and unique handle before full access
       //if email exist then return a refreshToken
-      console.log('THIS RUN AFTER SUCCESS');
+      console.log("THIS RUN AFTER SUCCESS");
       console.log(`This is profile id ${profile.id}`);
-      console.log('-----------------------');
+      console.log(`This is access token ${accessToken}`);
+      console.log(`This is refresh token ${refreshToken}`);
+      console.log("-----------------------");
+      profile.accessToken = accessToken;
+      profile.refreshToken = refreshToken;
       return cb(null, profile);
       //return profile id to serilize function above
     }
--- server.js
+++ server.js
@@ -1,53 +1,60 @@
-const express = require('express');
-const cors = require('cors');
-const passport = require('passport');
-require('./passport-init');
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+require("./passport-init");
 
 //PRIVATE SERVER DEV CONFIGS
 
-const PORT = 3111;
+const PORT = 4000;
 
 //END CONFIGS
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(passport.initialize());
 // app.use(passport.session());
 
-app.get('/', (req, res) => res.send('this is default page'));
-app.get('/fail', (req, res) => res.send('login failed'));
-app.get('/main', (req, res) => {
-  res.send('login successs');
+app.get("/", (req, res) => res.send("this is default page"));
+app.get("/fail", (req, res) => res.send("login failed"));
+app.get("/main", (req, res) => {
+  res.send("login successs");
 });
 
-app.get('/test', (req, res) => {
-  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+app.get("/test", (req, res) => {
+  var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
   console.log(ip);
-  res.send('testttt');
+  res.send("testttt");
 });
 
-app.get('/success', (req, res) => {
-  res.send('success');
+app.get("/success", (req, res) => {
+  res.send("success");
 });
 
 app.get(
-  '/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
+  "/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+    accessType: "offline",
+    approvalPrompt: "force",
+  })
 );
 
 app.get(
-  '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/fail' }),
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/fail" }),
   (req, res) => {
     console.log(req.ip);
-    console.log(req.headers['user-agent']);
-    console.log('THIS IS BEFORE REDIRECTVVVV');
+    console.log(req.headers["user-agent"]);
+    console.log("THIS IS BEFORE REDIRECTVVVV");
     console.log(req.user);
-    console.log('THIS IS BEFORE REDIRECT^^^^');
-    res.setHeader('Authorization', 'IF YOU SEE THIS, THE TEST IS COMPLETE');
+    console.log("THIS IS BEFORE REDIRECT^^^^");
+    res.setHeader("Authorization", "IF YOU SEE THIS, THE TEST IS COMPLETE");
     res.json(req.user);
   }
 );
